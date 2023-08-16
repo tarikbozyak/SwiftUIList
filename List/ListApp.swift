ef94d04bfb9cfffdfa9774f6e415d9f4dc415a07
Tarik Bozyak <tarik.bozyak@sabah.com.tr>
2023-08-16 21:56:08 +0300

    first commit

---
+++ List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Ahmed Tarık Bozyak on 9.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PersonListView()
+        }
+    }
+}
